6a31ef1a8f359e3538b3c0b206d03f40407ff0a8
DAVID LUCAS DOS SANTOS <25132827@dsn127m1246730.cfp127aluno.local>
2025-03-19 17:13:37 -0300

    Trapézio Add

--- src/br/com/engsenai/projeto_calculoDeForma/Main.java
+++ src/br/com/engsenai/projeto_calculoDeForma/Main.java
@@ -1,16 +1,11 @@
 package br.com.engsenai.projeto_calculoDeForma;
 
-import br.com.engsenai.projeto_calculoDeFormas.model.Quadrado;
 
 public class Main {
-
-	
 	
 	public static void main(String[] args) {
 		
-		Quadrado q1 = new Quadrado();
-		q1.lado = 2;
-		q1.mostrarDados();
 		
 	}
+	
 }
--- src/br/com/engsenai/projeto_calculoDeFormas/model/Quadrado.java
+++ src/br/com/engsenai/projeto_calculoDeFormas/model/Quadrado.java
@@ -2,4 +2,13 @@ package br.com.engsenai.projeto_calculoDeFormas.model;
 
 public class Quadrado {
 
+	private double lado;
+	
+	public void setLado(double lado) {
+		this.lado = lado;
+	}
+	
+	private double calcularArea() {
+		return lado * lado;
+	}
 }
